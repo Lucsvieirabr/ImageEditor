639732a64dc07f6ee254c0eab4e1fa53be323e5f
Lucsvieirabr <62450294+Lucsvieirabr@users.noreply.github.com>
2020-11-17 08:17:34 -0300

    JS ...

--- main.js
+++ main.js
@@ -1,2 +1,17 @@
 
+ // Check for the File API support.
+ if (window.File && window.FileReader && window.FileList && window.Blob) {
+    
+    setconfigson();
+    
+  } else {
+    // alert('The File APIs are not fully supported in this browser.');
+  }
 
+
+  function setconfigson(){
+
+   document.getElementsByClassName("menu").style.display="block";
+
+
+  }
