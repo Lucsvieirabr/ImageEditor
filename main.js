de6faeea4ed988e3c99b67f3f8b5576013ef6df4
Lucsvieirabr <62450294+Lucsvieirabr@users.noreply.github.com>
2020-11-17 19:50:06 -0300

    js

--- main.js
+++ main.js
@@ -6,10 +6,11 @@ if (window.File && window.FileReader && window.FileList && window.Blob) {
   // alert('The File APIs are not fully supported in this browser.');
 }
 
+  var evet;
 
+  function setconfigson(evt){
 
-  function setconfigson(){
-
+   evet = evt
    var menu = document.getElementById("menu");
    menu.style.setProperty('display', 'block');
    
@@ -17,10 +18,28 @@ if (window.File && window.FileReader && window.FileList && window.Blob) {
 
   function EditImage(){
 
-    var IMAGE = document.getElementsByClassName("file-input");
-    document.getElementsByClassName("main").innerHTML = IMAGE;
-    
-    
-
-
+    var mycanvas = document.getElementById("mycanvas");
+    var ctx = mycanvas.getContext("2d");
+    var f = evet.target.files[0]; // FileList object
+    var reader = new FileReader();
+    // Closure to capture the file information.
+    reader.onload = (function(theFile) {
+      return function(e) {
+        var binaryData = e.target.result;
+        //Converting Binary Data to base 64
+        var base64String = window.btoa(binaryData);
+        //showing file converted to base64
+        document.getElementById('img').setAttribute("src", "data:image/png;base64,"+base64String+"");
+        // document.getElementById('base64').value = base64String;
+        document.getElementById("mycanvas").setAttribute("display", "block");
+        
+      };
+    })(f);
+    // Read in the image file as a data URL.
+    reader.readAsBinaryString(f);
   }
+    
+    
+
+
+  
