ecba293282bd35900a230ed1a945c51221617c7e
Lucsvieirabr <62450294+Lucsvieirabr@users.noreply.github.com>
2020-11-17 12:41:30 -0300

    Script - Css

--- main.js
+++ main.js
@@ -1,19 +1,24 @@
 
- // Check for the File API support.
- if (window.File && window.FileReader && window.FileList && window.Blob) {
-    
-    setconfigson();
-    
-  } else {
-    // alert('The File APIs are not fully supported in this browser.');
-  }
+if (window.File && window.FileReader && window.FileList && window.Blob) {
+  document.querySelector('#file-js input[type=file]').addEventListener('change', setconfigson, false);
+  
+} else {
+  // alert('The File APIs are not fully supported in this browser.');
+}
+
 
 
   function setconfigson(){
 
    var menu = document.getElementById("menu");
    menu.style.setProperty('display', 'block');
+   
+  }
 
+  function EditImage(){
+
+    var IMAGE = document.getElementsByClassName("file-input");
+    
 
 
   }
